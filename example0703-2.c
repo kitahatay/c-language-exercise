1f5e30ff4e2614acbd31d269639939fb0ca97108
kitahatay@gmail.com <kitahatay@gmail.com>
2019-07-08 22:07:10 +0900

    文字を並べ替える関数追加

--- example0703-2.c
+++ example0703-2.c
@@ -1,8 +1,7 @@
-//引数の文字列を反転させて出力する。
-
 #include <stdio.h>
 
 char *reverse(char *);
+char *sort(char *);
 
 void main(int argc, char *argv[]) {
 
@@ -11,9 +10,26 @@ void main(int argc, char *argv[]) {
     p = argc > 1 ? argv[1] : "";
     printf("Input --> %s\n", argv[1]);
     printf("Output --> %s\n", reverse(p));
+    printf("Output --> %s\n", sort(p));
+}
+
+//引数の文字列を文字コード順に並べ替える。
+char *sort(char *str) {
+    int i;
+    int num=3;
+    char tmp[3];
+    
+    for(i=1;i<num;i++) {
+      if(strcmp(*str[i-1], *str[i])>0) {
+        strcpy(tmp, *str[i-1]);
+        strcpy(*str[i-1], *str[i]);
+        strcpy(*str[i], tmp);
+      }
+    }
 
 }
 
+//引数の文字列を反転させる。
 char *reverse(char *str) {
 //    printf("size --> %ld\n", sizeof(str));
 //   if (*str) {
